f32f93d33d3f9201eae6fc0bd4b7e27cd8c0edc5
Fasamii <fasami0512@gmail.com>
2026-02-03 21:48:31 +0100

    rust comment markers

--- after/queries/rust/injections.scm
+++ after/queries/rust/injections.scm
@@ -1,5 +1,35 @@
 ;; extends
 
+; comment maker in foos (sql)
+(
+ (block_comment) @_comment
+ . (_ (string_content) @injection.content)
+ (#lua-match? @_comment "sql")
+ (#set! injection.language "sql")
+)
+; comment maker in declarations (sql)
+(
+  (block_comment) @_comment
+  . value: (_ (string_content) @injection.content)
+  (#lua-match? @_comment "sql")
+  (#set! injection.language "sql")
+)
+
+; comment marker in foos (lua)
+(
+ (block_comment) @_comment
+ . (_ (string_content) @injection.content)
+ (#lua-match? @_comment "lua")
+ (#set! injection.language "lua")
+)
+; comment marker in declarations
+(
+  (block_comment) @_comment
+  . value: (_ (string_content) @injection.content)
+  (#lua-match? @_comment "lua")
+  (#set! injection.language "lua")
+)
+
 ; sqlx query (sql)
 (call_expression
   function: (scoped_identifier
@@ -16,17 +46,18 @@
   (#set! injection.combined)
 )
 
+; FIXME: LSP semantic tokens overriding sql hi groups
 ; sqlx query macro (sql)
 (macro_invocation
   macro: (scoped_identifier
-	path: (identifier) @sqlx_path
-    name: (identifier) @sqlx_macro)
+	path: (identifier) @_path
+    name: (identifier) @_macro)
   (token_tree
 	(_
 	  (string_content) @injection.content))
-
-  (#eq? @sqlx_path "sqlx")
-  (#eq? @sqlx_macro "query")
+  (#eq? @_path "sqlx")
+  (#eq? @_macro "query")
   (#set! injection.language "sql")
   (#set! injection.combined)
 )
+
